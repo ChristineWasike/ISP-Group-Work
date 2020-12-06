d133d7888cfe26228327691448550496393ac202
ChristineWasike <christinewasike22@gmail.com>
2020-12-06 17:37:53 +0300

    Implemented threads

--- elevator.c
+++ elevator.c
@@ -1,85 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #include <unistd.h>
 #define MAX_PEOPLE 10
 #define MAX_FLOORS 8
 
 //globals
-int elevator_state; // -1 (stop), 0(up), 1(down)
-int floor_number; // current floor
-int floor_numbers[10]; // array for people's destination floors
-int i; 
-pthread_t tid[10];
+int elevator_state;            // -1 (stop), 0(up), 1(down)
+int no_of_floors;              // current floor
+int floor_numbers[MAX_PEOPLE]; // array for people's destination floors
+int i;
+pthread_t person_tid[MAX_PEOPLE];
+pthread_t elevator_tid;
+pthread_mutex_t lock;
 
 // to sort floor numbers
-int cmpfunc(const void * a, const void * b) {
-    return ( *(int*)a - *(int*)b );
+int cmpfunc(const void *a, const void *b){
+    return (*(int *)a - *(int *)b);
 }
 
 // elevator simulation
-int elevator() {
-    // int * floor_number = (int*) calloc(1, sizeof(int));
+void *elevator(){
 
-    // to get random floor numbers ***LOOK IT UP***
+    pthread_mutex_lock(&lock);
+
+    // to get random floor numbers
     time_t t;
-    srand((unsigned) time(&t));
+    srand((unsigned)time(&t));
 
     // initializing elevator state to start at floor 1
     elevator_state = -1;
-    floor_number = 1;
-
-    // loading people - all starting at floor 1
-    for (i = 0; i < MAX_PEOPLE; i++) {
-        floor_numbers[i] = 1;
-    }
+    no_of_floors = 1;
 
     // generating random destinations for people
-    for (i = 0; i < MAX_PEOPLE; i++) {
+    for (i = 0; i < MAX_PEOPLE; i++){
         int temp;
         temp = rand() % (MAX_FLOORS - 1); // gets random numbers between 0 and 6
-        floor_numbers[i] = temp + 2; // adjusts random nums to (2 - 8)
+        floor_numbers[i] = temp + 2;      // adjusts random nums to (2 - 8)
     }
 
     // sorting floor numbers to ascending order
     qsort(floor_numbers, 10, sizeof(int), cmpfunc);
 
     // elevator movement
-    while (floor_number <= MAX_FLOORS) {
+    while (no_of_floors <= MAX_FLOORS){
 
-        printf("\nThe elevator is at floor %d.\n", floor_number);
+        printf("\nThe elevator is at floor %d.\n", no_of_floors);
         elevator_state = 0; // start moving
 
-        // [2, 2, 4, 5, 6, 7]
-        // Possible use of threads
-        // Add a thread that check that the condition in this for loop is satisfied
-        // at it end, go back to the thread that changes the floor levels
-        for (i = 0; i < MAX_PEOPLE; i++) {
-            if (floor_numbers[i] == floor_number) { // if person's destination is reached
+        for (i = 0; i < MAX_PEOPLE; i++)
+        {
+            if (floor_numbers[i] == no_of_floors)
+            {                        // if person's destination is reached
                 elevator_state = -1; // stop moving
-                printf("Person %d has arrived.\n", i + 1); // offload passenger
-                
+                sleep(1);
+                printf("Person %d with thread id %ld has arrived.\n", i + 1, person_tid[i]); // offload passenger
             }
         }
-        sleep(1);
         elevator_state = 0; // keep moving
-        floor_number++;
+        no_of_floors++;
     }
-
     elevator_state = -1; // if 8th floor is reached, end simulation
     printf("\nThe simulation has ended!\n");
 
+    pthread_mutex_unlock(&lock);
 }
 
-int main() {
+int main(){
+    int err;
 
-    for (int i = 0; i < 9; i++) {
-        pthread_create(&tid, NULL, elevator, (void *)&tid);
+    if (pthread_mutex_init(&lock, NULL) != 0){
+        printf("\nMutex init has failed\n");
+        return -1;
     }
 
-    // for (int i = 1; i <= 10; i++)
-        // pthread_join(tid[i], NULL);
+    pthread_create(&elevator_tid, NULL, elevator, NULL);
 
-    elevator();
-    
+    for (int i = 0; i < 10; i++){
+        err = pthread_create(&person_tid[i], NULL, elevator, NULL);
+        if (err != 0)
+            printf("\nCannot create thread: [%s]", strerror(err));
+    }
+
+    for (int i = 0; i < 10; i++){
+        pthread_join(person_tid[i], NULL);
+    }
+
+    pthread_exit(NULL);
+
+    pthread_mutex_destroy(&lock);
+
+    return 0;
 }
